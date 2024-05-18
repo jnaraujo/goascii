ebe1f7f0c046b80c8687f3960c966eb00ccf31a2
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-05-18 13:31:40 -0300

    chore: Remove unused code and comments in convert/main.go

--- cmd/convert/main.go
+++ cmd/convert/main.go
@@ -11,13 +11,6 @@ import (
 func main() {
 	pwd, _ := os.Getwd()
 
-	// f, err := os.Create(pwd + "/cpu.prof")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// pprof.StartCPUProfile(f)
-	// defer pprof.StopCPUProfile()
-
 	imgPath := pwd + "/test/cat.jpg"
 
 	file, err := os.Open(imgPath)
@@ -32,14 +25,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// for i := 0; i < 100; i++ {
-	// 	file.Seek(0, 0)
-	// 	_, err = imgtoascii.Convert(file)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
 
 	os.WriteFile(pwd+"/test/cat.txt", []byte(ascii), 0644)
 }
